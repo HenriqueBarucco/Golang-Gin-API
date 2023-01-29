7d2397b76b77431585e286e5104e2d787e08677c
Henrique Barucco <henrique_barucco@hotmail.com>
2023-01-29 11:55:59 -0300

    fear: Primeira rota com Gin

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Henrique Barucco",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
+
+	r.Run()
+}
