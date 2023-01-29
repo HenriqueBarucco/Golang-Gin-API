ba608f2ba44ee779e0fc6ca484709bd61e8db81a
Henrique Barucco <henrique_barucco@hotmail.com>
2023-01-29 12:50:14 -0300

    feat: Rotas HTTP

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/henriquebarucco/Golang-Gin-API/database"
-	"github.com/henriquebarucco/Golang-Gin-API/models"
 	"github.com/henriquebarucco/Golang-Gin-API/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Henrique", CPF: "00000000001", RG: "1000000000"},
-		{Nome: "Teste", CPF: "00000000002", RG: "2000000000"},
-		{Nome: "Barucco", CPF: "00000000003", RG: "3000000000"},
-	}
 	routes.HandlerRequests()
 }
